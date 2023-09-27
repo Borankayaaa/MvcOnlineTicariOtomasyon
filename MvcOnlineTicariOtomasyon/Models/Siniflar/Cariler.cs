34e6fa310c6aecc7af032d8d917d1a253142afbb
Boran Kaya <borankaya210@gmail.com>
2023-09-27 21:28:45 +0300

    Cariler bolgu tamamlandı.

--- MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/Cariler.cs
@@ -13,11 +13,12 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         public int Cariid { get; set; }
 
         [Column(TypeName = "Varchar")]
-        [StringLength(30)]
+        [StringLength(30,ErrorMessage ="En Fazla 30 Karakter Yazabilirsiniz.")]
         public string CariAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [Required(ErrorMessage ="Bu Alanı Boş Geçemezsiniz.")]
         public string CariSoyad { get; set; }
 
         [Column(TypeName = "Varchar")]
@@ -27,7 +28,7 @@ namespace MvcOnlineTicariOtomasyon.Models.Siniflar
         [Column(TypeName = "Varchar")]
         [StringLength(50)]
         public string CariMail { get; set; }
-
+        public bool Durum { get; set; }
 
         public ICollection<SatisHareket> SatisHarekets { get; set; }
     }
